761506aecb46ffae8a7dd37739bcdc3bdcbbc6a2
Henry Ren <renxg126@126.com>
2020-04-12 17:46:00 +0800

    sonartest

---
+++ child-parent/child-two/src/main/java/com/henry/childtwo/Test.java
@@ -0,0 +1,10 @@
+package com.henry.childtwo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test child two...");
+	}
+
+}
